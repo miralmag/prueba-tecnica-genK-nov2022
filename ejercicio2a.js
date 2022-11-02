33ce8ec2d7aae16ad03017dd49edff8358df6222
miralmag <miriam.almagro@gmail.com>
2022-11-02 18:19:07 +0100

    create multiplication function (exercise 2a)

---
+++ ejercicio2a.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const array1 = [
+    [1, 3, 5],
+    [7, 9, 7],
+    [5, 3, 1],
+];
+
+function multiplication (){
+const array2 = [
+    [2, 4, 6],
+    [8, 10, 8],
+    [6, 4, 2],
+    ];
+    
+const flat1 = array1.flat();
+let flat2 = [];
+
+for (let i=0; i<array2.length; i++){
+    for(let j= 0; j<array2[i].length; j++){
+    flat2.push(array2[j][i])
+    }
+}
+const result = []
+
+for (let i = 0; i<flat1.length; i++){
+    result[i] = flat1[i] * flat2[i]
+}
+
+return result;
+
+}
